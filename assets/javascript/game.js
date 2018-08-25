a83c2006a45e700984ef0aeecfcede6282b6871f
PANDAmonium <jazuma86@yahoo.com>
2018-08-24 21:59:45 -0700

    psuedo coded my javascript and added an array and variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,66 @@
+// Word list
+var selectableWords =   
+    [
+        "seahawks",
+        "athlete",
+        "playoffs",
+        "MVP",
+        "championship",
+        "goal",
+        "halftime",
+        "olympics",
+        "sportsmanship",
+        "mariners",
+        "marathon",
+    ];
 
+// Maximum number of guesses
+const maxGuesses = 10;            
+// Stores the letters guessed
+var lettersGuessed = [];       
+// Index of the current word in the array
+var currentWord;
+// Word we build to match the current word           
+var guessingWord = [];   
+// How many tries the player has left
+var guessesRemaining = 0;
+// Alert to tell if the game has started       
+var gameStarted = false;  
+// Alert for "press any key to try again"           
+var hasFinished = false;
+// Total wins    
+var wins = 0;                   
+
+// Reset our game variables
+
+// Use Math.floor to round the random number down
+
+// Clear out arrays
+
+// Clear sports image
+
+// Create the guessing word and clear it out
+
+// Hide win and gameover images/text
+
+// Show display
+
+//  Update the display on the HTML Page
+
+// Update the image based on how many guesses
+
+// If a game is finished, delete one keystroke and reset.
+
+// Check to make sure a-z was pressed.
+
+// To make sure this letter has not been used yet
+
+// This function takes a letter and finds all instances it was used in the string and replaces them in the guess word.
+
+// Array to store positions of letters in string
+
+// Loop through word finding all instances of guessed letter, store the indicies in an array.
+
+// If there are no indicies, remove a guess and update the sports image
+
+// Loop through all the indicies and replace the "_" with a letter.
